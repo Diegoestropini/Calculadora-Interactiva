3d9356d75d8f93a4a50cbf8643dfff80abff5d14
Diego Estropini <86741336+Diegoestropini@users.noreply.github.com>
2025-07-20 18:47:48 -0300

    Add operation tests

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,51 @@ test('Clear button resets the display', () => {
   userEvent.click(screen.getByText(/Clear/i));
   expect(display.textContent).toBe('');
 });
+
+test('addition works', () => {
+  const { container } = render(<App />);
+  const display = container.querySelector('.input');
+
+  userEvent.click(screen.getByText('2'));
+  userEvent.click(screen.getByText('+'));
+  userEvent.click(screen.getByText('3'));
+  userEvent.click(screen.getByText('='));
+
+  expect(display.textContent).toBe('5');
+});
+
+test('subtraction works', () => {
+  const { container } = render(<App />);
+  const display = container.querySelector('.input');
+
+  userEvent.click(screen.getByText('9'));
+  userEvent.click(screen.getByText('-'));
+  userEvent.click(screen.getByText('4'));
+  userEvent.click(screen.getByText('='));
+
+  expect(display.textContent).toBe('5');
+});
+
+test('multiplication works', () => {
+  const { container } = render(<App />);
+  const display = container.querySelector('.input');
+
+  userEvent.click(screen.getByText('5'));
+  userEvent.click(screen.getByText('*'));
+  userEvent.click(screen.getByText('6'));
+  userEvent.click(screen.getByText('='));
+
+  expect(display.textContent).toBe('30');
+});
+
+test('division works', () => {
+  const { container } = render(<App />);
+  const display = container.querySelector('.input');
+
+  userEvent.click(screen.getByText('8'));
+  userEvent.click(screen.getByText('/'));
+  userEvent.click(screen.getByText('2'));
+  userEvent.click(screen.getByText('='));
+
+  expect(display.textContent).toBe('4');
+});
